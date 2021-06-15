f59d771a90c37d0b59056193a9cfb0733e517348
unknown <behradk90@gmail.com>
2021-06-15 11:58:02 +0100

    Adjust file input box using bootstrap, adding image deletability through edit form as well as some basic image cropping for better user exp.

--- controllers/campgrounds.js
+++ controllers/campgrounds.js
@@ -1,4 +1,5 @@
 const Campground = require('../models/campground');
+const { cloudinary } = require("../cloudinary")
 const wrapAsync = require('../utilities/wrapAsync');
 
 module.exports.index = async (req, res) => {
@@ -53,6 +54,12 @@ module.exports.updateCampground = async (req, res) => {
     const imgs = req.files.map(f => ({ url: f.path, filename: f.filename }));
     campground.images.push(...imgs);
     await campground.save();
+    if (req.body.deleteImages) {
+        for (let filename of req.body.deleteImages) {
+            await cloudinary.uploader.destroy(filename);
+        }
+        await campground.updateOne({ $pull: { images: { filename: { $in: req.body.deleteImages } } } });
+    }
     req.flash('success', 'Update successful!')
     res.redirect(`/campgrounds/${campground._id}`)
 }
